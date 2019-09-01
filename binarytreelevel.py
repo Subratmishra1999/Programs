7cdea6a3f118e73785f399483ede3c21596f9ad9
Subratmishra1999 <35842967+Subratmishra1999@users.noreply.github.com>
2019-09-01 13:49:18 +0530

    Update binarytreelevel.py

--- binarytreelevel.py
+++ binarytreelevel.py
@@ -1,3 +1,6 @@
+'''Program to print the level of each node in a binary tree having distinct node values'''
+
+
 class Node: 
     def __init__(self,key): 
         self.left = None
@@ -18,3 +21,29 @@ def insert(root,node):
                 root.left = node 
             else: 
                 insert(root.left, node) 
+
+def findlevel(a,c,d):
+    d[a.val]=c
+    if a.left is None:
+        pass
+    else:
+        findlevel(a.left,c+1,d)
+    if a.right is None:
+        pass
+    else:
+        findlevel(a.right,c+1,d)
+    
+
+
+n=int(input())
+l=[int(_) for _ in input().split()]
+a=Node(l[0])
+for i in range(1,n):
+    insert(a,Node(l[i]))
+d={}
+findlevel(a,1,d)
+m=[]
+for i in l:
+    m.append(d[i])
+print(*m)
+
